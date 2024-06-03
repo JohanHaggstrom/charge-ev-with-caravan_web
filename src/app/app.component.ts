71b7bc720f17f1bcfa7b94658acec5501d7831bb
Johan Häggström <johan.haggstrom@gmail.com>
2024-06-03 20:39:51 +0200

    Added clickable link to google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterOutlet } from '@angular/router';
 import { CaravanChargePoint, IdentifiedCaravanChargePoint } from './app.model';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MatIconModule, MatButtonModule, MatTooltipModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
